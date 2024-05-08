52cf352cb7eea4c29bf50cc4018118100cd83ae3
Lucelest <143048016+luccsss@users.noreply.github.com>
2024-05-08 17:16:57 -0500

    Update QuickSelect.cpp

--- Ordenamientos/QuickSelect.cpp
+++ Ordenamientos/QuickSelect.cpp
@@ -1,8 +1,9 @@
+#include "pch.h"
 #include <iostream>
 //En general tiene una complejidad O(n), pero puede llegar a ser en el peor de los casos : O(n^2)
 using namespace std;
 
-int particion(int *A, int p, int r) {
+int particion(int* A, int p, int r) {
 	int x = A[r]; //el pivote
 	int i = p - 1; //indice de los menores
 	for (int j = p; j < r; j++) {
@@ -15,19 +16,16 @@ int particion(int *A, int p, int r) {
 	return i + 1;
 }
 
-int quickselect(int *A, int p, int r, int k) {
+int quickselect(int* A, int p, int r, int k) {
 	if (p == r) return A[p];
 	//indice del pivote con A ordenado Izq(Menores) Der(Mayores) al pivote
-	int q = particion(A, p, r); 
-	int l = q - p + 1; //nro elementos del sub arreglo donde se encuentra el kesimo elemento
-	if (k == l)
-		return A[q];
-	else if (k < l) {
-		return quickselect(A, p, q - 1, k);
+	if (p < r) {
+		int q = particion(A, p, r);
+		//int l = q - p + 1; //nro elementos del sub arreglo donde se encuentra el kesimo elemento
+		quickselect(A, p, q - 1, k);
+		quickselect(A, q + 1, r, k);
 	}
-	else {
-		return quickselect(A, q + 1, r, k - l);
-	}
+	return A[k-1]; 
 }
 
 int main() {
@@ -36,7 +34,7 @@ int main() {
 	int n = 9;
 	int k = 5; //el kesimo menor elemento del arreglo
 
-	int e = quickselect(arreglo, 0, n - 1, k); 
+	int e = quickselect(arreglo, 0, n - 1, k);
 
 	for (int i = 0; i < n; i++) {
 		cout << arreglo[i] << " ";
