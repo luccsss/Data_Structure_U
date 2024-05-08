ba4529d7e55d792c38884b4aa73170fb64082763
Lucelest <143048016+luccsss@users.noreply.github.com>
2024-05-07 23:32:32 -0500

    Update MergeSort.cpp

--- Ordenamientos/MergeSort.cpp
+++ Ordenamientos/MergeSort.cpp
@@ -5,40 +5,40 @@
 using namespace System;
 using namespace std;
 
-void Merge(int *A1, int *A2, int *A, int n) {
+void Merge(int* A1, int* A2, int* A, int n) {
 	int i = 0, j = 0, k = 0;
 	int mitad = n / 2;
 	while (i < mitad && j < n - mitad) {
 		if (A1[i] < A2[j]) {
 			A[k] = A1[i];
-			i++;k++;
+			i++; k++;
 		}
 		else {
 			A[k] = A2[j];
-			j++;k++;
+			j++; k++;
 		}
 	}
 	while (i < mitad) {
 		A[k] = A1[i];
-		i++;k++;
+		i++; k++;
 	}
 	while (j < n - mitad) {
 		A[k] = A2[j];
-		j++;k++;
+		j++; k++;
 	}
 }
 
-void mergeSort(int *A, int n) {
+void mergeSort(int* A, int n) {
 	if (n > 1) {
 		int mitad = n / 2;
-		int *A1 = new int[mitad];
-		int *A2 = new int[n-mitad];
+		int* A1 = new int[mitad];
+		int* A2 = new int[n - mitad];
 
-		for (int i = 0;i < mitad;i++) {
+		for (int i = 0; i < mitad; i++) {
 			A1[i] = A[i];
 		}
-		for (int i = mitad;i < n;i++) {
-			A2[i-mitad] = A[i];
+		for (int i = mitad; i < n; i++) {
+			A2[i - mitad] = A[i];
 		}
 		mergeSort(A1, mitad);
 		mergeSort(A2, n - mitad);
@@ -48,6 +48,13 @@ void mergeSort(int *A, int n) {
 
 int main()
 {
-    system("pause");
-    return 0;
+	int *a = new int[8]{10, 5, 2, 6, 8, 9, 1, 3};
+	int n = 8;
+	mergeSort(a, n); 
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i]<<" ";
+	}
+	system("pause");
+	return 0;
 }
