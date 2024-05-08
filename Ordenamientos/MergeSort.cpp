7c68f3bde179420f8933958e7fcce89c387c937a
Lucelest <143048016+luccsss@users.noreply.github.com>
2024-05-07 20:21:11 -0500

    Update MergeSort.cpp

--- Ordenamientos/MergeSort.cpp
+++ Ordenamientos/MergeSort.cpp
@@ -1,3 +1,53 @@
-Merge(t[] a1, t[ ] a2, t[] a, entero n){
-  
+//Su complejidad generalmente es => nlog(n)
+#include <iostream>
+#include <conio.h>
+
+using namespace System;
+using namespace std;
+
+void Merge(int *A1, int *A2, int *A, int n) {
+	int i = 0, j = 0, k = 0;
+	int mitad = n / 2;
+	while (i < mitad && j < n - mitad) {
+		if (A1[i] < A2[j]) {
+			A[k] = A1[i];
+			i++;k++;
+		}
+		else {
+			A[k] = A2[j];
+			j++;k++;
+		}
+	}
+	while (i < mitad) {
+		A[k] = A1[i];
+		i++;k++;
+	}
+	while (j < n - mitad) {
+		A[k] = A2[j];
+		j++;k++;
+	}
+}
+
+void mergeSort(int *A, int n) {
+	if (n > 1) {
+		int mitad = n / 2;
+		int *A1 = new int[mitad];
+		int *A2 = new int[n-mitad];
+
+		for (int i = 0;i < mitad;i++) {
+			A1[i] = A[i];
+		}
+		for (int i = mitad;i < n;i++) {
+			A2[i-mitad] = A[i];
+		}
+		mergeSort(A1, mitad);
+		mergeSort(A2, n - mitad);
+		Merge(A1, A2, A, n);
+	}
+}
+
+int main()
+{
+    system("pause");
+    return 0;
 }
