6d0c267a216049439c006ce47f1d7d270283ef15
Lucelest <143048016+luccsss@users.noreply.github.com>
2024-05-07 20:15:25 -0500

    Create QuickSort.cpp

---
+++ Ordenamientos/QuickSort.cpp
@@ -0,0 +1,16 @@
+//Su complejidad es generalmente lineal, pero puede llegar a ser cuadrática en el peor de los casos.
+//#Ejemplo 1
+//Complejidad lineal:
+
+#include <iostream>
+#include <conio.h>
+
+using namespace System;
+using namespace std;
+
+
+int main()
+{
+    system("pause");
+    return 0;
+}
