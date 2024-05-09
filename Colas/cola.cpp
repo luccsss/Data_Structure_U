5a6b6e85f8aa562041c43bd9cb360f7d6eb86a19
Lucelest <143048016+luccsss@users.noreply.github.com>
2024-05-08 23:07:29 -0500

    Create cola.cpp

---
+++ Colas/cola.cpp
@@ -0,0 +1,64 @@
+#include "Nodo.h"
+#include <iostream>
+using namespace std;
+
+template<class T>
+class Cola {
+private:
+	Nodo<T>* inicio;
+	Nodo<T>* fin;
+	size_t longitud;
+
+public:
+	Cola() : inicio(NULL), fin(NULL), longitud(0) {}
+
+	void enqueue(T valor) {
+		Nodo<T>* nuevoNodo = new Nodo<T>(valor);
+		if (estaVacia()) {
+			inicio = nuevoNodo;
+			fin = inicio;
+		}
+		else {
+			fin->siguiente = nuevoNodo;
+			fin = nuevoNodo;
+		}
+		longitud++;
+		nuevoNodo = NULL;
+	}
+
+
+	T dequeue() {
+		if (estaVacia()) return NULL;
+		T dato = inicio->valor;
+
+		if (inicio == fin) {
+			delete inicio;
+			inicio = NULL;
+			fin = NULL;
+		}
+		else {
+			Nodo<T>* aux = inicio;
+			inicio = inicio->siguiente;
+			delete aux;
+		}
+		longitud--;
+		return dato;
+	}
+
+	bool estaVacia() { return this->longitud == 0; }
+	size_t getLongitud() { return longitud; }
+
+	Nodo<T>* getNodoEnPosicion(int posicion) {
+		if (posicion < 0 || estaVacia()) throw "Posición inválida o cola vacía";
+
+		int contador = 0;
+		Nodo<T>* actual = inicio;
+
+		while (actual != NULL) {
+			if (contador == posicion) return actual;
+			contador++;
+			actual = actual->siguiente;
+		}
+		throw "Posición fuera de rango";
+	}
+};
