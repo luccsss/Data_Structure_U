b30d753e0eb4851f95c23423e2c3cf2821fbf24c
Lucelest <143048016+luccsss@users.noreply.github.com>
2024-06-05 14:53:31 -0500

    Update and rename heapMax.cpp to heapMax&Min.cpp

--- heap/heapMax&Min.cpp
+++ heap/heapMax&Min.cpp
@@ -25,13 +25,31 @@ void maxHeapify(int A[], int n, int i) {
 		maxHeapify(A, n, largest);
 	}
 }
-
+void minHeapify(int A[], int n, int i) {
+	int l = left(i);
+	int r = rigth(i);
+	int smallest = i;
+	if (l < n && A[l] < A[i]) {
+		smallest = l;
+	}
+	if (r < n && A[r] < A[smallest]) {
+		smallest = r;
+	}
+	if (smallest != i) {
+		swap(A[i], A[smallest]);
+		minHeapify(A, n, smallest);
+	}
+}
 void buildMaxHeap(int A[], int n) {
 	for (int i = n / 2 - 1;i >= 0;--i) {
 		maxHeapify(A, n, i);
 	}
 }
-
+void buildMinHeap(int A[], int n) {
+	for (int i = n / 2 - 1; i >= 0; --i) {
+		minHeapify(A, n, i);
+	}
+}
 void print(int A[], int n) {
 	for (int i = 0;i < n;++i) {
 		cout << A[i] << " ";
@@ -40,10 +58,19 @@ void print(int A[], int n) {
 }
 
 //aplicación de ordenamiento
-void heapsort(int A[], int n) {
+void heapsortMax(int A[], int n) {
 	buildMaxHeap(A, n);
 	for (int i = n - 1;i > 0;--i) {
 		swap(A[0], A[i]);
 		maxHeapify(A, --n, 0);
 	}
 }
+
+void heapsortMin(int A[], int n) {
+	buildMinHeap(A, n);
+	for (int i = n - 1; i > 0; --i) {
+		swap(A[0], A[i]);
+		minHeapify(A, --n, 0);
+	}
+}
+
